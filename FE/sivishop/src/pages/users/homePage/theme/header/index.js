572cee7a32f5c79f187cd63fe50276621ab08443
lehuunhan <lehuunhan529@gmail.com>
2024-06-08 20:55:22 +0700

    create header-top

--- FE/sivishop/src/pages/users/homePage/theme/header/index.js
+++ FE/sivishop/src/pages/users/homePage/theme/header/index.js
@@ -1,11 +1,64 @@
-import { memo } from 'react'
+import { memo } from 'react';
+import { 
+    AiOutlineFacebook, 
+    AiOutlineInstagram, 
+    AiOutlineLinkedin, 
+    AiOutlineGlobal,
+    AiOutlineUser,
+    AiOutlineMail      
+} from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import { fomater } from 'utils/fomater'
+import './style.scss';
 
 const Header = () => {
-    return (
-        <>
-            <h1>Header</h1>
-        </>
-    );
+    return <div className='header_top'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6 header_top_left'>
+                    <ul>
+                        <li>
+                            <AiOutlineMail />
+                            <span>hello@gmail.com</span>
+                        </li>
+                        <li>
+                            Miễn phí ship đơn từ {fomater(200000)}
+                        </li>
+                    </ul>
+                </div>
+                <div className='col-6 header_top_right'>
+                    <ul>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineFacebook />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineInstagram />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineLinkedin />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineGlobal  />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineUser   />
+                                <span>Đăng nhập</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default memo(Header)
